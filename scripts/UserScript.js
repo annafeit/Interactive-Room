7ade4d80ff37cfa077417e47964b9f30e73b412e
Anna Feit <anna.schwein@freenet.de>
2011-10-06 08:55:01 +0200

    helper functions for intersection tests

--- scripts/UserScript.js
+++ scripts/UserScript.js
@@ -2,17 +2,18 @@ var User={};
 
 Kata.require([
 	'katajs/oh/GraphicsScript.js',
+	kata_base_offset + 'scripts/utils.js',
 ], function() {
 	/**
-	 * Simulate inheritance from GraphicsScript by defining a super variable
-	 * This is the first Object in the OH and therefore inherits from 
-	 * GraphicsScript and not from Script!
-	 */
-	var SUPER = Kata.GraphicsScript.prototype;		
+	* Simulate inheritance from GraphicsScript by defining a super variable
+	* This is the first Object in the OH and therefore inherits from
+	* GraphicsScript and not from Script!
+	*/
+	var SUPER = Kata.GraphicsScript.prototype;
 	var space;
 	/**
-	 * Constructor
-	 */
+	* Constructor
+	*/
 	User.UserScript = function(channel, args){
 		//save the xml3d element
 		var t = document.getElementsByTagName("xml3d");
@@ -39,86 +40,85 @@ Kata.require([
 		this.connect(args, null, Kata.bind(this.connected, this));
 	};
 	/**
-	 * Simulate inheritance from GraphicsScript by extending the User class with the methods of the parent(SUPER) class
-	 */
+	* Simulate inheritance from GraphicsScript by extending the User class with the methods of the parent(SUPER) class
+	*/
 	Kata.extend(User.UserScript, SUPER);
 	
 	/**
-	 * proximity callback TODO ???
-	 */ 
+	* proximity callback TODO ???
+	*/
     User.UserScript.prototype.proxEvent = function(remote, added) {
         if (added){
-        	Kata.warn("Camera Discover object.");
+         Kata.warn("Camera Discover object.");
             this.presence.subscribe(remote.id());
         }
         else{
-        	Kata.warn("Camera wiped object");
-        }            
-    };	    
-	
-	/** Camera sync */
+         Kata.warn("Camera wiped object");
+        }
+    };
+
+/** Camera sync */
     User.UserScript.prototype.syncCamera = function() {
         var now = new Date();
         this.setCameraPosOrient(this.presence.predictedPosition(now),
                                 this.presence.predictedOrientation(now),
-                                0.1);	//lag:0.1 just to match the code...
-        this.checkWalls();
-        
+                                0.1); //lag:0.1 just to match the code...
+        //this.checkWalls(); -> doesn't work yet
     };
     
-    /** 
-     * Creates the room, the user wants to login to
-     */
-    User.UserScript.prototype.createRoom = function(){    	
-    	this.createObject(kata_base_offset + "scripts/RoomScript.js", 
-	   			"Room.RoomScript", 
-				{ space:this.space,
-				  visual:{mesh:this.roomMesh},
-				  loc:{scale: "1.0"}	//just to match the code..
-				});    
+    /**
+* Creates the room, the user wants to login to
+*/
+    User.UserScript.prototype.createRoom = function(){
+	    this.createObject(kata_base_offset + "scripts/RoomScript.js",
+		"Room.RoomScript",
+		{ space:this.space,
+		visual:{mesh:this.roomMesh},
+		loc:{scale: "1.0"} //just to match the code..
+		});
     }
     
     /**
-     * Sets the camera to the "door-view"
-     */
+* Sets the camera to the "door-view"
+*/
     User.UserScript.prototype.setCamToDoor = function(){
-    	var views = document.getElementsByTagName("view");
-    	var view;
-    	
-    	//finds the viewpoint at the door
-    	for (var i = 0; i<views.length; i++){
-    		view = views[i];
-    		if (view.id.substr(0,4) == "door"){
-    			break;
-    		}
-    	}
-    	var now = new Date();
- 		var loc = this.presence.predictedLocationAtTime(now);
- 		var p = view.position;
- 		var o = view.orientation;
- 		
- 		loc.pos = [p.x, p.y, p.z];
- 		var or = Kata._helperQuatFromAxisAngle([o.axis.x, o.axis.y, o.axis.z], o.angle);
- 		loc.orient = or; 
- 		
- 		this.presence.setLocation(loc);		
+	     var views = document.getElementsByTagName("view");
+	     var view;
+	    
+	     //finds the viewpoint at the door
+	     for (var i = 0; i<views.length; i++){
+	    	 view = views[i];
+	    	 if (view.id.substr(0,4) == "door"){
+	    		 break;
+	    	 }
+	     }
+	     var now = new Date();
+	     var loc = this.presence.predictedLocationAtTime(now);
+	     var p = view.position;
+	     var o = view.orientation;
+	 
+	     loc.pos = [p.x, p.y, p.z];
+	     var or = Kata._helperQuatFromAxisAngle([o.axis.x, o.axis.y, o.axis.z], o.angle);
+	     loc.orient = or;
+	 
+	     this.presence.setLocation(loc);
     };
     
     User.UserScript.prototype.setCam = function(pos){
     	var now = new Date();
- 		var loc = this.presence.predictedLocationAtTime(now); 		
- 		loc.pos = pos; 	 		
- 		this.presence.setLocation(loc);		
+    	var loc = this.presence.predictedLocationAtTime(now);
+    	loc.pos = pos;
+    	this.presence.setLocation(loc);
     };
     
 	/**
-	 * Callback that is triggered when object is connected to the space	  
-	 */
-	User.UserScript.prototype.connected = function(presence, space, reason){		
+	* Callback that is triggered when object is connected to the space
+	*/
+	User.UserScript.prototype.connected = function(presence, space, reason){
 		//handle connection failure
 		if (presence == null){
-			Kata.error('Failed to connect viewer to '+ space+'. Reason: ' + reason);
-			throw "error";
+		Kata.error('Failed to connect viewer to '+ space+'. Reason: ' + reason);
+		throw "error";
 		}
 		
 		//save world presence
@@ -128,42 +128,82 @@ Kata.require([
 		this.enableGraphicsViewport(presence,0);
 		
 		presence.setQueryHandler(Kata.bind(this.proxEvent, this));
-        presence.setQuery(0);                  		
-		
+        presence.setQuery(0);
+
         //save the activeView
-        var id = this.xml3d.getAttribute("activeview");
-        this.camera = $(id)[0];
-        
+        var id = this.xml3d.activeView;
+        this.camera = document.getElementById(id);
+		        
 		//create Room of user
-		this.createRoom();				  
-		
+		this.createRoom();
+
         //set up camera sync
         this.mCamUpdateTimer = setInterval(Kata.bind(this.syncCamera, this), 60);
         this.syncCamera();
         
 		//display username
 		document.getElementById("name").innerHTML=username;
-	}; 
-	
+	};
+
 	/**
-	 * check if the camera is out of the room and make walls invisible if this is true.
-	 */
+	* check if the camera is out of the room and make walls invisible if this is true.
+	*/
 	User.UserScript.prototype.checkWalls = function(){
-		var x = Math.round(this.xml3d.offsetWidth/2);
-		var y = Math.round(this.xml3d.offsetHeight/2);
-		var obj = this.xml3d.getElementByPoint(x,y);
+		//create ray with origin in camera and direction in camera direction
+		var ray = this.xml3d.createXML3DRay();
+		ray.origin = this.camera.position;		
+		ray.direction = this.camera.getDirection();
+		//create ray with origin in camera and direction in opposite camera direction
+		var rayNeg = this.xml3d.createXML3DRay();		
+		rayNeg.origin = this.camera.position;
+		rayNeg.direction = this.camera.getDirection().negate();
+				
+		var rt1 = Helper.rayIntersectsScene(ray);
+		var rt2 = Helper.rayIntersectsScene(rayNeg);
+		if (!(rt1 && rt2)){
+			//outside of the room (not a wall on both sides of the camera)			
+			setShaderTransparent();							
+		}
+		else{
+			setShaderSolid();
+		}
 	}
 	
-	//Enum for Keycode
-    User.UserScript.prototype.Keys = {
-	        UP : 38,
-	        DOWN : 40,
-	        LEFT : 37,
-	        RIGHT : 39
-	};
+
+	function setShaderTransparent(){
+		var groups = document.getElementsByTagName("group");		
+		for (var i =0;i<groups.length;i++)
+		{
+			var obj = groups[i];
+			if(obj.getAttribute("type") == "wall" || obj.getAttribute("type") == "ceiling"){
+				obj.setAttribute("shader", "#transparentMaterial" );				
+			}
+		}
+	}
 	
+	function setShaderSolid(){	
+		var groups = document.getElementsByTagName("group");		
+		for (var i =0;i<groups.length;i++)
+		{
+			var obj = groups[i];
+			if(obj.getAttribute("type") == "wall" || obj.getAttribute("type") == "ceiling"){
+				obj.setAttribute("shader", "#material" );				
+			}
+		}
+	}
+	
+		
+	
+	//Enum for Keycode
+	User.UserScript.prototype.Keys = {
+		UP : 38,
+		DOWN : 40,
+		LEFT : 37,
+		RIGHT : 39
+	};
+
 	//Handle messages from GUI
-	User.UserScript.prototype._handleGUIMessage = function (channel, msg) {		
+	User.UserScript.prototype._handleGUIMessage = function (channel, msg) {
 		if(msg.msg=="loaded" && msg.mesh==this.roomMesh){
 			this.setCamToDoor();
 		}
@@ -171,14 +211,14 @@ Kata.require([
 		if(msg.msg == "keyup"){
 			this.keyIsDown[msg.keyCode] = false;
 			
-			//If neither up nor down key is now pressed -> don't go 
+			//If neither up nor down key is now pressed -> don't go
 			if ( !this.keyIsDown[this.Keys.UP] && !this.keyIsDown[this.Keys.DOWN])
-                this.presence.setVelocity([0, 0, 0]);
+			    this.presence.setVelocity([0, 0, 0]);
 			//If neither left nor right key is pressed -> don't rotate
-            if ( !this.keyIsDown[this.Keys.LEFT] && !this.keyIsDown[this.Keys.RIGHT])
-                this.presence.setAngularVelocity(Kata.Quaternion.identity());
-		}
-		if (msg.msg == "keydown"){		
+			if ( !this.keyIsDown[this.Keys.LEFT] && !this.keyIsDown[this.Keys.RIGHT])
+			    this.presence.setAngularVelocity(Kata.Quaternion.identity());
+			}
+		if (msg.msg == "keydown"){
 			var now = new Date();
 			var preOrient = this.presence.predictedOrientation(now);
 			var origOrient = new Kata.Quaternion();
@@ -188,7 +228,7 @@ Kata.require([
 			origOrient[3] = preOrient[3];
 			var avMat = Kata.QuaternionToRotation(origOrient);
             var avSpeed = 50;
-            var full_rot_seconds = 10.0;	            	          
+            var full_rot_seconds = 10.0;
             
             var avXX = avMat[0][0] * avSpeed;
             var avXY = avMat[0][1] * avSpeed;
@@ -202,75 +242,73 @@ Kata.require([
            
             if (this.keyIsDown[this.Keys.UP]) {
             	if (this.ctrl){
-            		/*
-            		var dir = this.camera.getDirection();
-            		var up = this.camera.getUpVector();
-            		var axis = up.cross(dir);
-            		this.presence.setAngularVelocity(
-    	                    Kata.Quaternion.fromAxisAngle([dir.x, dir.y, dir.z], 2.0*Math.PI/full_rot_seconds)
-    	                );*/
-            		
-            		
-            		//new rotation N = inv(O) * V * O 
-            		//FIXME still doesn't work correctly?
-            		var inv = origOrient.inverse();
-            		var up = Kata.Quaternion.fromAxisAngle([1, 0, 0], 0.017); //0.017 ca. 5°
-            		
-            		var rotateBack = inv.multiply(origOrient);
-            		var rotateVert = up.multiply(rotateBack);
-            		//TODO find out why not reversed (the function call)
-                    var res = origOrient.multiply(up);
-            		//TODO how to make it smooth (like Velocity)            		
-            		this.presence.setOrientation(res);
-            	}
-            	else{
-            		this.presence.setVelocity([-avZX, -avZY, -avZZ]);
-            	}
+				    /*
+					var dir = this.camera.getDirection();
+					var up = this.camera.getUpVector();
+					var axis = up.cross(dir);
+					this.presence.setAngularVelocity(
+					Kata.Quaternion.fromAxisAngle([dir.x, dir.y, dir.z], 2.0*Math.PI/full_rot_seconds)
+					);*/ //new rotation N = inv(O) * V * O
+		            //FIXME still doesn't work correctly?
+		            var inv = origOrient.inverse();
+		            var up = Kata.Quaternion.fromAxisAngle([1, 0, 0], 0.017); //0.017 ca. 5°
+		            
+		            var rotateBack = inv.multiply(origOrient);
+		            var rotateVert = up.multiply(rotateBack);
+		             //TODO find out why not reversed (the function call)
+		            var res = origOrient.multiply(up);
+		             //TODO how to make it smooth (like Velocity)
+		            this.presence.setOrientation(res);
+		            }
+	             else{
+	            	this.presence.setVelocity([-avZX, -avZY, -avZZ]);
+	             }
                 
             }
             if (this.keyIsDown[this.Keys.DOWN]) {
             	if (this.ctrl){
-            		//new rotation N = inv(O) * V * O 
-            		var inv = origOrient.inverse();            		
+            		//new rotation N = inv(O) * V * O
+            		var inv = origOrient.inverse();
             		var up = Kata.Quaternion.fromAxisAngle([1, 0, 0], -0.017); //0.017 ca. 1°
-            		
-            		var rotateBack = origOrient.multiply(inv);
-            		var rotateVert = rotateBack.multiply(up)
-                    var res = origOrient.multiply(up);
-            		
-            		this.presence.setOrientation(res);
+		            
+		            var rotateBack = origOrient.multiply(inv);
+		            var rotateVert = rotateBack.multiply(up)
+		            var res = origOrient.multiply(up);
+		            
+		            this.presence.setOrientation(res);
             	}
-            	else{
-            		this.presence.setVelocity([avZX, avZY, avZZ]);
-            	}
+	            else{
+	            	this.presence.setVelocity([avZX, avZY, avZZ]);
+	            }
             }
             
             if (this.keyIsDown[this.Keys.LEFT]) {
-            	if (this.ctrl){	            	
-	                this.presence.setAngularVelocity(
-	                    Kata.Quaternion.fromAxisAngle([0, 1, 0], 2.0*Math.PI/full_rot_seconds)
-	                );
+            	if (this.ctrl){
+            		this.presence.setAngularVelocity(
+            				Kata.Quaternion.fromAxisAngle([0, 1, 0], 2.0*Math.PI/full_rot_seconds)
+            		);
             	}
             	else{
             		this.presence.setVelocity([-avXX, -avXY, -avXZ]);
-            	}	                
+            	}
             }
             if (this.keyIsDown[this.Keys.RIGHT]) {
             	if (this.ctrl){
-	                this.presence.setAngularVelocity(
-	                    Kata.Quaternion.fromAxisAngle([0, 1, 0], -2.0*Math.PI/full_rot_seconds)
-	                );
+            		this.presence.setAngularVelocity(
+            				Kata.Quaternion.fromAxisAngle([0, 1, 0], -2.0*Math.PI/full_rot_seconds)
+            		);
             	}
             	else{
             		this.presence.setVelocity([avXX, avXY, avXZ]);
             	}
             }
 		}
-		
+	
 		this.updateGFX(this.presence);
-		
+	
 	};
-	
-	
-	
+
+
+
 }, kata_base_offset + "scripts/UserScript.js");
+
---
+++ scripts/utils.js
@@ -0,0 +1,79 @@
+var Helper={};
+
+Kata.require([ 
+	
+    ],function() {
+		/**
+		 * tests whether two objects intersect by their bounding boxes
+		 */
+		Helper.objectIntersectsObject= function(obj1, obj2){
+			//if (obj1 instanceof XML3DObject (?) && obj2 instanceof XML3DObject
+			var bb1=obj1.getBoundingBox();
+			var bb2=obj2.getBoundingBox();
+			
+			if( bb1.min.x>bb2.max.x || bb1.min.y > bb2.max.y  || bb1.min.z > bb2.max.z ||
+			    bb2.min.x>bb1.max.x || bb2.min.y > bb1.max.y  || bb2.min.z > bb1.max.z 	){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+		
+		/**
+		 * test if a ray intersects an object
+		 */
+		Helper.rayIntersectsObject = function(obj1, ray){
+			if (obj1.getBoundingBox){
+				var bb = obj1.getBoundingBox();		//TODO getBoundingBox not implemented for browser
+				var o = ray.origin;
+				var d = ray.direction;
+				var tnear = -(Number.MAX_VALUE);
+				var tfar = Number.MAX_VALUE;
+				for (var i = 0; i<3; i++){
+					if (d[i] == 0){
+						//parallel to plane that's normal to x/y/z axis
+						if (o[i] < bb.min[i] || o[i] > bb.max[i]){
+							//can't intersect with box
+							return false;
+						}
+					}
+					else{
+						//not parallel
+						var tmpnear = (bb.min[i] - o[i]) / d[i];
+						var tmpfar = (bb.max[i] - o[i]) / d[i];
+						if (tmpnear > tmpfar) {
+							var tmp = tmpnear;
+							tmpnear = tmpfar;
+							tmpfar = tmp;	//tmpnear indicates the intersection distances to the nearest plane					
+						}
+						if (tmpnear > tnear) tnear = tmpnear;				//tnear indicates the greatest intersection point with near plane in any dimension
+						if (tmpfar < tfar) tfar = tmpfar;					//tnear indicates the smallest intersection point with far plane in any dimension
+						if (tfar <0) {										//box is behind camera
+							return false;
+						}
+					}
+				}
+				if (tnear < tfar){
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+			
+		}
+		
+		Helper.rayIntersectsScene = function(ray){
+			var objs = document.getElementsByTagName("group");
+			for (var i = 0; i<objs.length; i++){
+				var obj = objs[i];
+				
+				if(this.rayIntersectsObject(obj,ray)){
+					return true;
+				}
+			
+			}
+			return false;
+		}
+});
